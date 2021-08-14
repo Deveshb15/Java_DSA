a4b86b2ec6d56d50c9979e441c8a129e46426a53
Deveshb15 <deveshb583@gmail.com>
2021-08-14 22:31:02 +0530

    misc questions

---
+++ Questions/Array/src/com/devesh/ProductExceptSelf.java
@@ -0,0 +1,31 @@
+package com.devesh;
+
+public class ProductExceptSelf {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+
+        int[] ans = productExceptSelp(nums);
+
+        System.out.println(ans);
+    }
+
+    static int[] productExceptSelp(int[] nums) {
+        int[] right = new int[nums.length];
+        int prod = 1;
+        for (int i = nums.length-1; i >= 0 ; i--) {
+            prod *= nums[i];
+            right[i] = prod;
+        }
+
+        int[] ans = new int[nums.length];
+        prod = 1;
+        for (int i = 0; i < nums.length-1; i++) {
+            int lp = prod;
+            ans[i] = lp * right[i+1];
+            prod *= nums[i];
+        }
+        ans[nums.length-1] = prod;
+
+        return ans;
+    }
+}
