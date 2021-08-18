ab4f4dfef9fe88dd339d87f0f691f201f2349338
Deveshb15 <deveshb583@gmail.com>
2021-08-18 14:55:13 +0530

    peak mountain question

---
+++ Searching/BinarySearch/src/Questions/PeakMountain.java
@@ -0,0 +1,26 @@
+package Questions;
+
+public class PeakMountain {
+    public static void main(String[] args) {
+        int[] arr = {24,69,100,99,79,78,67,36,26,19};
+        System.out.println(findPeak(arr));
+    }
+
+    public static int findPeak(int[] nums) {
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start < end){
+//            int mid = (start+end)/2;  //(start+end) will exceed range of int in java
+            int mid = start + (end - start) / 2;
+
+            if(nums[mid] > nums[mid+1]){
+                end = mid;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
